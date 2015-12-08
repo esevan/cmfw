95a47e177933624fc3ccac2c07c039c167af1bf1
esevan <esevan.park@gmail.com>
2015-12-08 08:58:06 +0000

    day 2

--- src/comm_core.cpp
+++ src/comm_core.cpp
@@ -1103,7 +1103,7 @@ void OPEL_Server::generic_read_handler(uv_work_t *req)
 				}
 			}
 			/* Greeting? */
-			wirte(new_client_fd, "hoho", 5);
+			write(new_client_fd, "hoho", 5);
 		}
 	}while(0);
 
