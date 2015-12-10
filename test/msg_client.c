49f994370002409a9178f26f0e0ddf7b5202411d
esevan <esevan.park@gmail.com>
2015-12-10 08:57:26 +0000

    day 4

--- test/msg_client.c
+++ test/msg_client.c
@@ -7,9 +7,6 @@
 
 char *intf_name = "Test Interface";
 OPEL_Client *cli = NULL;
-struct timeval init_time;
-struct timeval tmp_time;
-struct timeval conn_time;
 
 void onConnect(OPEL_MSG *op_msg, int status);
 void onRead(OPEL_MSG *op_msg, int status);
@@ -27,56 +24,60 @@ int main()
 
 void onConnect(OPEL_MSG *op_msg, int status)
 {
-	gettimeofday(&conn_time, NULL);
-	if(conn_time.tv_usec-init_time.tv_usec < 0)
-		printf("%ld.%ld]OnConnect\n", conn_time.tv_sec-init_time.tv_sec-1, 1000000+conn_time.tv_usec-init_time.tv_usec);
-	else
-		printf("%ld.%ld]OnConnect\n", conn_time.tv_sec-init_time.tv_sec, conn_time.tv_usec-init_time.tv_usec);
+	printf("OnConnect:\n")
 	if(!status){
-		char send_str[] = "First Msg from client";
-		printf("Connect Succedded\n");
+	/////////////////////////////////////////////////////
+		printf("\tConnect Succedded\n");
+	
+		char send_str[] = "Msg from client";
 		cli->msg_write(send_str, strlen(send_str)+1);
+	////////////////////////////////////////////////////
 	}
 	else{
-		printf("Connect Failed(%d)\n", status);
+		printf("\tConnect Failed(%d)\n", status);
 		delete cli;
 		cli = new OPEL_Client(intf_name, onRead, onConnect);
 	}
 }
 void onRead(OPEL_MSG *op_msg, int status)
 {
-	printf("OnRead\n");
+	printf("OnRead:\n");
 	if(NULL == op_msg){
-		printf("OP_MSG = NULL\n");
+		printf("\tOP_MSG = NULL\n");
 		return;
 	}
 	if(!status){
-		printf("Got Msg:%s\n", (char *)op_msg->get_data());
-		if(!strcmp((char *)op_msg->get_data(), "Message for ack test")){
-			char send_str[] = "Ack message from client";
-			printf("Sending %s\n", send_str);
+		printf("\tGot Msg:%s\n", (char *)op_msg->get_data());
+		//////////////////////////////////////////////////////////////////
+		if(!strcmp((char *)op_msg->get_data(), "REQ write test from server")){
+			char send_str[] = "Ack from client";
+			printf("\t\tSend: %s\n", send_str);
 			cli->msg_write(send_str, strlen(send_str)+1, op_msg, NULL);
 		}
+		//////////////////////////////////////////////////////////////////
 		else{
 			char send_str[] = "Req message from client";
-			printf("Sending %s\n", send_str);
+			printf("\t\tSend: %s\n", send_str);
 			cli->msg_write(send_str, strlen(send_str)+1, NULL, onAck);
 		}
+		//////////////////////////////////////////////////////////////////
 	}
 	else{
-		printf("Read Failed(%d)\n", status);
+		printf("\tRead Failed(%d)\n", status);
 		exit(1);
 	}
 }
 void onAck(OPEL_MSG *op_msg, int status)
 {
-	printf("OnAck called\n");
+	printf("OnAck:\n");
 	if(!status && NULL != op_msg){
-		printf("Got data : %s\n", (char *)op_msg->get_data());
-		char send_str[] = "Server req&ack test";
-		printf("Send :%s\n", send_str);
+		//////////////////////////////////////////////////////////////////
+		printf("\tGot data : %s\n", (char *)op_msg->get_data());
+		char send_str[] = "Client req&ack test done";
+		printf("\t\tSend :%s\n", send_str);
 		cli->msg_write(send_str, strlen(send_str)+1);
+		//////////////////////////////////////////////////////////////////
 	}
 	else
-		printf("Failed\n");
+		printf("\tFailed\n");
 }
--- test/msg_server.c
+++ test/msg_server.c
@@ -8,65 +8,61 @@
 char *intf_name = "Test Interface";
 OPEL_Server *ser = NULL;
 
-void default_read_handler(OPEL_MSG *op_msg, int status);
-void ack_handler(OPEL_MSG *op_msg, int status);
-void file_ack_handler(OPEL_MSG *op_msg, int status);
+void onRead(OPEL_MSG *op_msg, int status);
+void onAck(OPEL_MSG *op_msg, int status);
+void onAck2(OPEL_MSG *op_msg, int status);
 
 int main()
 {
-	ser = new OPEL_Server(intf_name, default_read_handler);
+	ser = new OPEL_Server(intf_name, onRead);
 
 	uv_run(uv_default_loop(), UV_RUN_DEFAULT);
-	sleep(30);
 
 	return 0;
 }
-void default_read_handler(OPEL_MSG *op_msg, int status)
+void onRead(OPEL_MSG *op_msg, int status)
 {
 	static int test_cnt = 0;
-	printf("default_read_hnadler\n");
+	printf("Server:OnRead\n");
 
 	if(NULL == op_msg){
 		printf("OP_msg = NULL\n");
 		exit(1);
 	}
 
-	printf("get_data:%s\n", (char *)op_msg->get_data());
-
-	if(!strcmp("Req message from client", (char *)op_msg->get_data())){
-		char send_str[] = "Message for server's reply";
-		printf("ack message test :%s\n", send_str);
+	printf("\tGot data:%s\n", (char *)op_msg->get_data());
+	
+	////////////////////////////////////////////////////////////////////
+	if(!strcmp("REQ from client", (char *)op_msg->get_data())){
+		char send_str[] = "ACK from server";
+		printf("\t\tsend:%s\n", send_str);
 		ser->msg_write(send_str, strlen(send_str)+1, op_msg, NULL);
 	}
+	////////////////////////////////////////////////////////////////////
 	else if(0 == test_cnt){
-		char send_str[] = "Message for server's sending";
-		printf("send message test:%s\n", send_str);
+		char send_str[] = "MSG write test from server";
+		printf("\t\tsend:%s\n", send_str);
 		ser->msg_write(send_str, strlen(send_str)+1);
 		test_cnt++;
 	}
+	////////////////////////////////////////////////////////////////////
 	else if(1 == test_cnt){
-		char send_str[] = "Message for ack test";
-		printf("send & ack message test:%s\n", send_str);
-		ser->msg_write(send_str, strlen(send_str)+1, NULL, ack_handler);
+		char send_str[] = "REQ write test from server";
+		printf("\t\tsend:%s\n", send_str);
+		ser->msg_write(send_str, strlen(send_str)+1, NULL, onAck);
 		test_cnt++;
 	}
+	////////////////////////////////////////////////////////////////////
 }
 
-void ack_handler(OPEL_MSG *op_msg, int status)
+void onAck(OPEL_MSG *op_msg, int status)
 {
-	printf("ack_handler called\n");
+	printf("Server:OnAck\n");
 	if(!status && NULL != op_msg){
-		printf("Got data : %s\n", (char *)op_msg->get_data());
-		printf("Sending file...");
-		ser->file_write("../msg", NULL, file_ack_handler);
+		printf("\t\tGot data : %s\n", (char *)op_msg->get_data());
 	}
 	else
-		printf("Failed\n");
+		printf("\t\tFailed\n");
 
-}
-
-void file_ack_handler(OPEL_MSG *op_msg, int status)
-{
-	printf("File send done!\n");
 }
 
