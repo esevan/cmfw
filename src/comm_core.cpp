57243459ac60aef62ab37973e94f56b55ba0ed10
esevan <esevan.park@gmail.com>
2015-12-11 09:22:26 +0000

    day 5

--- src/comm_core.cpp
+++ src/comm_core.cpp
@@ -2562,7 +2562,7 @@ void OPEL_Client::generic_read_handler(uv_work_t *req)
 				*/
 
 				op_client->rqs->signal(op_msg->get_req_id(), queue_data);
-				comm_log("Ack comes:");
+				comm_log("Ack comes:%s", op_msg->get_data());
 /*
 				if(req_err < 0){
 					if(!queue_data->attached) delete queue_data;
@@ -3011,6 +3011,7 @@ void OPEL_Client::after_ra_handler(uv_work_t *req, int status)
 		}
 		else{
 			op_msg = queue_data->op_msg;
+			comm_log("%s Ack Comes", op_msg->get_data());
 			if(queue_data->handler)
 				queue_data->call_handler();
 			else if(NULL != op_client->client_handler)
