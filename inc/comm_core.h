2d1855c0b9acfebe76ce2976f18cf1f263b23895
esevan <esevan.park@gmail.com>
2015-12-30 05:22:42 +0000

    151230

--- inc/comm_core.h
+++ inc/comm_core.h
@@ -33,8 +33,8 @@
 #endif
 
 #define MAX_NAME_LEN 16
-#define MAX_MSG_LEN 880
-#define MAX_DAT_LEN 880
+#define MAX_MSG_LEN 512
+#define MAX_DAT_LEN 512
 #define MAX_REQ_LEN 32
 #define MAX_CLIENTS 30		//bit operation check
 #define REQ_TIMEOUT 10
